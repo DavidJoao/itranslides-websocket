12e1b2f4ec59acf27b04d202ba81bca14f700c1f
DavidJoao <davidsandoval596@gmail.com>
2024-11-28 15:46:02 -0800

    FIX'

--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-import express from "express"
-import { Server } from "socket.io"
-import { createServer } from "node:http"
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-const port = process.env.PORT ?? 3001
-
-const app = express()
-const server = createServer(app)
+const port = process.env.PORT || 3001;
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
-})
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
 app.use(express.json());
 
@@ -71,9 +70,9 @@ io.on("connection", socket => {
 })
 
 app.get("/", (req, res) => {
-	res.send("Hello World")
-})
+    res.send("Hello World");
+});
 
 server.listen(port, () => {
-	console.log(`Running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
